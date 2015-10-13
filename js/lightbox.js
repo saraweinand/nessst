e98a08daeaaa464af1d0f3e39293473b080ba4b6
saraweinand <saraweinand@gmail.com>
2015-10-13 15:06:13 -0700

    Version 3

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,313 +1,40 @@
-jQuery(document).ready(function($){
-//open popup
-  $('.01').on('click', function(event){
-    event.preventDefault();
-    $('.popup-01').addClass('is-visible');
+// Create a lightbox
+
+var $lightbox = $("<div class='lightbox'></div>");
+var $img = $("<img>");
+var $caption = $("<p class='caption'></p>");
+
+
+// Add image and caption to lightbox
+
+$lightbox
+  .append($img)
+  .append($caption);
+
+// Add lighbox to document
+
+$('body').append($lightbox);
+
+
+$('.gallery li').click(function (e) {
+  e.preventDefault();
+
+  // Get image link and description
+  var src = $(this).children('img').attr("src");
+  var cap = $(this).children('img').attr("alt");
+
+  // Add data to lighbox
+
+  $img.attr('src',src);
+  $caption.text(cap);
+
+  // $lightbox.append('<img src="' + src + '"></img><p class="caption">' + caption + '</p>');
+
+  // Show lightbox
+
+  $lightbox.show();
+
+  $lightbox.click(function () {
+    $lightbox.hide();
   });
-
-	 $('.01').on('click', function(event){
-	    event.preventDefault();
-	    $('article').addClass('noscroll');
-	  });
-  
-  $('.02').on('click', function(event){
-    event.preventDefault();
-    $('.popup-02').addClass('is-visible');
-  });
-
-	 $('.02').on('click', function(event){
-	    event.preventDefault();
-	    $('article').addClass('noscroll');
-	  });
-
-  $('.03').on('click', function(event){
-    event.preventDefault();
-    $('.popup-03').addClass('is-visible');
-  });
-
-	 $('.03').on('click', function(event){
-	    event.preventDefault();
-	    $('article').addClass('noscroll');
-	  });
-
-  
-  $('.04').on('click', function(event){
-    event.preventDefault();
-    $('.popup-04').addClass('is-visible');
-  });
-
-	 $('.04').on('click', function(event){
-	    event.preventDefault();
-	    $('article').addClass('noscroll');
-	  });
-
-  $('.05').on('click', function(event){
-    event.preventDefault();
-    $('.popup-05').addClass('is-visible');
-  });
-
-	 $('.05').on('click', function(event){
-	    event.preventDefault();
-	    $('article').addClass('noscroll');
-	  });
-
-  
-  $('.06').on('click', function(event){
-    event.preventDefault();
-    $('.popup-06').addClass('is-visible');
-  });
-
-	 $('.06').on('click', function(event){
-	    event.preventDefault();
-	    $('article').addClass('noscroll');
-	  });
-
-  $('.07').on('click', function(event){
-    event.preventDefault();
-    $('.popup-07').addClass('is-visible');
-  });
-
-	 $('.07').on('click', function(event){
-	    event.preventDefault();
-	    $('article').addClass('noscroll');
-	  });
-
-  
-  $('.08').on('click', function(event){
-    event.preventDefault();
-    $('.popup-08').addClass('is-visible');
-  });
-
-	 $('.08').on('click', function(event){
-	    event.preventDefault();
-	    $('article').addClass('noscroll');
-	  });
-
-  $('.09').on('click', function(event){
-    event.preventDefault();
-    $('.popup-09').addClass('is-visible');
-  });
-
-	 $('.09').on('click', function(event){
-	    event.preventDefault();
-	    $('article').addClass('noscroll');
-	  });
-
-  $('.10').on('click', function(event){
-    event.preventDefault();
-    $('.popup-10').addClass('is-visible');
-  });
-
-	 $('.10').on('click', function(event){
-	    event.preventDefault();
-	    $('article').addClass('noscroll');
-	  });
-
-  
-  $('.11').on('click', function(event){
-    event.preventDefault();
-    $('.popup-11').addClass('is-visible');
-  });
-
-	 $('.11').on('click', function(event){
-	    event.preventDefault();
-	    $('article').addClass('noscroll');
-	  });
-
-  $('.12').on('click', function(event){
-    event.preventDefault();
-    $('.popup-12').addClass('is-visible');
-  });
-
-	 $('.12').on('click', function(event){
-	    event.preventDefault();
-	    $('article').addClass('noscroll');
-	  });
-
-
-
-
-
-
-
-
-
-
-
-//close popup
-  $('.popup-01').on('click', function(event){
-    if( $(event.target).is('.popup-close, .popup-reserve') || $(event.target).is('.popup-01') ) {
-      event.preventDefault();
-      $(this).removeClass('is-visible');
-    }
-  });
-   
-
-		   $('.popup-close, .popup-01').click(function(){
-		    $('article').removeClass("noscroll");
-		});
-
-
-  $('.popup-02').on('click', function(event){
-    if( $(event.target).is('.popup-close, .popup-reserve') || $(event.target).is('.popup-02') ) {
-      event.preventDefault();
-      $(this).removeClass('is-visible');
-    }
-  });
-   
-
-		   $('.popup-close, .popup-02').click(function(){
-		    $('article').removeClass("noscroll");
-		});
-
-  $('.popup-03').on('click', function(event){
-    if( $(event.target).is('.popup-close, .popup-reserve') || $(event.target).is('.popup-03') ) {
-      event.preventDefault();
-      $(this).removeClass('is-visible');
-    }
-  });
-   
-
-		   $('.popup-close, .popup-03').click(function(){
-		    $('article').removeClass("noscroll");
-		});
-
-  $('.popup-04').on('click', function(event){
-    if( $(event.target).is('.popup-close, .popup-reserve') || $(event.target).is('.popup-04') ) {
-      event.preventDefault();
-      $(this).removeClass('is-visible');
-    }
-  });
-   
-
-		   $('.popup-close, .popup-04').click(function(){
-		    $('article').removeClass("noscroll");
-		});
-
-  $('.popup-05').on('click', function(event){
-    if( $(event.target).is('.popup-close, .popup-reserve') || $(event.target).is('.popup-05') ) {
-      event.preventDefault();
-      $(this).removeClass('is-visible');
-    }
-  });
-   
-
-		   $('.popup-close, .popup-05').click(function(){
-		    $('article').removeClass("noscroll");
-		});
-
-  $('.popup-06').on('click', function(event){
-    if( $(event.target).is('.popup-close, .popup-reserve') || $(event.target).is('.popup-06') ) {
-      event.preventDefault();
-      $(this).removeClass('is-visible');
-    }
-  });
-   
-
-		   $('.popup-close, .popup-06').click(function(){
-		    $('article').removeClass("noscroll");
-		});
-
-  $('.popup-07').on('click', function(event){
-    if( $(event.target).is('.popup-close, .popup-reserve') || $(event.target).is('.popup-07') ) {
-      event.preventDefault();
-      $(this).removeClass('is-visible');
-    }
-  });
-   
-
-		   $('.popup-close, .popup-07').click(function(){
-		    $('article').removeClass("noscroll");
-		});
-
-  $('.popup-08').on('click', function(event){
-    if( $(event.target).is('.popup-close, .popup-reserve') || $(event.target).is('.popup-08') ) {
-      event.preventDefault();
-      $(this).removeClass('is-visible');
-    }
-  });
-   
-
-		   $('.popup-close, .popup-08').click(function(){
-		    $('article').removeClass("noscroll");
-		});
-
-  $('.popup-09').on('click', function(event){
-    if( $(event.target).is('.popup-close, .popup-reserve') || $(event.target).is('.popup-09') ) {
-      event.preventDefault();
-      $(this).removeClass('is-visible');
-    }
-  });
-   
-
-		   $('.popup-close, .popup-09').click(function(){
-		    $('article').removeClass("noscroll");
-		});
-
-  $('.popup-10').on('click', function(event){
-    if( $(event.target).is('.popup-close, .popup-reserve') || $(event.target).is('.popup-10') ) {
-      event.preventDefault();
-      $(this).removeClass('is-visible');
-    }
-  });
-   
-
-		   $('.popup-close, .popup-10').click(function(){
-		    $('article').removeClass("noscroll");
-		});
-
-  $('.popup-11').on('click', function(event){
-    if( $(event.target).is('.popup-close, .popup-reserve') || $(event.target).is('.popup-11') ) {
-      event.preventDefault();
-      $(this).removeClass('is-visible');
-    }
-  });
-   
-
-		   $('.popup-close, .popup-11').click(function(){
-		    $('article').removeClass("noscroll");
-		});
-
-  $('.popup-12').on('click', function(event){
-    if( $(event.target).is('.popup-close, .popup-reserve') || $(event.target).is('.popup-12') ) {
-      event.preventDefault();
-      $(this).removeClass('is-visible');
-    }
-  });
-   
-
-		   $('.popup-close, .popup-12').click(function(){
-		    $('article').removeClass("noscroll");
-		});
-
-
-
-
-  //close popup when clicking the esc keyboard button
-  $(document).keyup(function(event){
-      if(event.which=='27'){
-        $('.popup').removeClass('is-visible');
-      }
-    });
 });
-
-$("#launch").click(function() {
-  $('.overlay').fadeIn();
-
-});
-
-$("#close, .popup-reserve, .overlay").click(function() {
-  $('.overlay').fadeOut();
-});
-
-$(".overlay").click(function() {
-  $('.overlay').fadeOut();
-  $('.popup-a').removeClass('is-visible');
-});
-
-
-
-
-
-
-
